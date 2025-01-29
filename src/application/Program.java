d1c9eff596ff8138c804deabf1aa84fa201f9c45
PombalGustavo <pombalgustavo@gmail.com>
2025-01-29 14:23:24 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 import org.w3c.dom.ls.LSOutput;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+        Seller seller = new Seller(13, "Bob", "Bob@gmail.com", new Date(), 3000.00, obj);
+        
+        System.out.println(seller);
 
     }
 }
