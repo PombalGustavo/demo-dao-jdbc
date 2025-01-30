12bc42d5fad81ca68c9e487c3c558956565d9c93
PombalGustavo <pombalgustavo@gmail.com>
2025-01-30 11:21:37 -0300

    Reusing imstantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -51,30 +51,42 @@ public class SellerDaoJDBC implements SellerDao {
             rs = st.executeQuery();
 
             if (rs.next()) {
-                Department dept = new Department();
-                dept.setId(rs.getInt("DepartmentId"));
-                dept.setName(rs.getString("DepName"));
+                Department dept = instantiateDepartment(rs);
 
-                Seller seller = new Seller();
-                seller.setId(rs.getInt("Id"));
-                seller.setName(rs.getString("Name"));
-                seller.setEmail(rs.getString("Email"));
-                seller.setBaseSalary(rs.getDouble("BaseSalary"));
-                seller.setBirthDate(rs.getDate("BirthDate"));
-                seller.setDepartment(dept);
+                Seller seller = instantiateSeller(rs, dept);
 
                 return seller;
             }
             return null;
 
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new DbException("Error: " + e.getMessage());
-        }
-        finally {
+        } finally {
             DB.closeStatement(st);
             DB.closeResultSet(rs);
         }
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dept) throws SQLException {
+        Seller seller = new Seller();
+
+        seller.setId(rs.getInt("Id"));
+        seller.setName(rs.getString("Name"));
+        seller.setEmail(rs.getString("Email"));
+        seller.setBaseSalary(rs.getDouble("BaseSalary"));
+        seller.setBirthDate(rs.getDate("BirthDate"));
+        seller.setDepartment(dept);
+
+        return seller;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dept = new Department();
+
+        dept.setId(rs.getInt("DepartmentId"));
+        dept.setName(rs.getString("DepName"));
+
+        return dept;
     }
 
     @Override
